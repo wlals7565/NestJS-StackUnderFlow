2445b002c1a538962298654ab67fb2d8f4be8d2c
wlals7565 <117993640+wlals7565@users.noreply.github.com>
2025-03-25 16:05:11 +0900

    [수정]: 이미지 경로 잘못 나오는 버그 수정

--- src/images/images.controller.ts
+++ src/images/images.controller.ts
@@ -40,35 +40,11 @@ export class ImagesController {
     file: Express.Multer.File,
     @GetUser() user: User,
   ) {
-    console.log('uploadImage');
-    const username = user.username;
-    // 파일 저장을 위한 디렉토리 경로 설정
-    const uploadDir = path.join(process.cwd(), 'assets', username); // process.cwd() 사용
-    const randomName = uuidv4();
-
-    // MIME 타입에 맞는 확장자 매핑
-    console.log(file.mimetype);
-    const [type, fileExtension] = file.mimetype.split('/');
-    if (type !== 'image') {
-      throw new BadRequestException('지원되지 않는 파일 형식입니다.');
+    if (!file) {
+      throw new BadRequestException('파일이 제공되지 않았습니다.');
     }
 
-    // 파일 경로
-    const filePath = path.join(uploadDir, `${randomName}${fileExtension}`);
-
-    // 폴더가 없으면 생성 (중간 폴더도 생성)
-    fs.mkdirSync(uploadDir, { recursive: true });
-
-    // 메모리에서 파일을 디스크에 저장
-    fs.writeFileSync(filePath, file.buffer);
-
-    const fileRelativePath = path.join(
-      'static',
-      username,
-      `${randomName}${fileExtension}`,
-    );
-
-    return { msg: 'ok', imagePath: fileRelativePath }; // 저장된 파일 경로 반환
+    return this.imagesService.uploadFile(file, user.username);
   }
 
   @Get()
--- src/images/images.service.ts
+++ src/images/images.service.ts
@@ -1,11 +1,43 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable } from '@nestjs/common';
 import { CreateImageDto } from './dto/create-image.dto';
 import { UpdateImageDto } from './dto/update-image.dto';
+import * as path from 'path';
+import * as fs from 'fs';
+import { v4 as uuidv4 } from 'uuid';
 
 @Injectable()
 export class ImagesService {
+  private readonly baseUploadPath = path.join(process.cwd(), 'assets');
+
   create(createImageDto: CreateImageDto) {
     return 'This action adds a new image';
+  }
+
+  async uploadFile(file: Express.Multer.File, username: string) {
+    const uploadDir = path.join(this.baseUploadPath, 'images', username);
+    const randomName = uuidv4();
+    const [type, fileExtension] = file.mimetype.split('/');
+
+    if (type !== 'image') {
+      throw new BadRequestException('지원되지 않는 파일 형식입니다.');
+    }
+
+    const filePath = path.join(uploadDir, `${randomName}.${fileExtension}`);
+
+    // 디렉토리 생성
+    fs.mkdirSync(uploadDir, { recursive: true });
+
+    // 파일 저장
+    fs.writeFileSync(filePath, file.buffer);
+
+    const fileRelativePath = path.join(
+      'static',
+      'images',
+      username,
+      `${randomName}.${fileExtension}`,
+    );
+
+    return { msg: 'ok', imagePath: fileRelativePath };
   }
 
   findAll() {
