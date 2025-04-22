10f342893b6d40bdadef06969398d6bf6807929b
wlals7565 <117993640+wlals7565@users.noreply.github.com>
2025-04-22 14:46:44 +0900

    [구현]: 알람

--- src/notification/notification.controller.ts
+++ src/notification/notification.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, UseGuards, Get } from '@nestjs/common';
+import { Controller, UseGuards, Get, Delete, Param } from '@nestjs/common';
 import NotificationService from './notification.service';
 import { JwtAuthGuard } from 'src/auth/jwt-auth.guard';
 import { User } from 'src/common/types/user.type';
@@ -13,5 +13,19 @@ export default class NotificationController {
   async getAllAlarms (@GetUser() user: User) {
     const allAlarms = await this.notificationService.getAllAlarms(user.uuid);
     return allAlarms
+  }
+
+  // 전체 알람 삭제
+  @UseGuards(JwtAuthGuard)
+  @Delete()
+  async deleteAllAlarms (@GetUser() user: User) {
+    return await this.notificationService.deleteAllAlarms(user.uuid);
+  }
+
+  // 하나의 알람 삭제
+  @UseGuards(JwtAuthGuard)
+  @Delete("/:alarmId")
+  async deleteAlarm (@GetUser() user: User, @Param('alarmId') alarmId: string) {
+    return await this.notificationService.deleteAlarm(user.uuid, alarmId)
   }
 }
--- src/notification/notification.service.ts
+++ src/notification/notification.service.ts
@@ -1,4 +1,4 @@
-import { HttpException, Injectable, InternalServerErrorException } from '@nestjs/common';
+import { BadRequestException, HttpException, Injectable, InternalServerErrorException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import Alarm from 'src/alarm/entities/alarm.entity';
 import { Repository } from 'typeorm';
@@ -21,6 +21,32 @@ export default class NotificationService {
         throw error;
       }
       throw new InternalServerErrorException();
+    }
+  }
+
+  async deleteAllAlarms(userId: string) {
+    try {
+      await this.AlarmRepository.softDelete({notifiedUser: {id: userId}});
+      return {message: "성공적으로 모든 소식을 삭제하였습니다."};
+    } catch (error) {
+      if(error instanceof HttpException) {
+        throw error;
+      }
+      throw new InternalServerErrorException;
+    }
+  }
+
+  async deleteAlarm(userId: string, alarmId: string) {
+    try {
+      const result = await this.AlarmRepository.softDelete({notifiedUser: {id: userId}, id: alarmId})
+      if(!result.affected) {
+        throw new BadRequestException("존재하지 않거나 이미 삭제된 소식을 삭제할 수는 없습니다.")
+      }
+    } catch (error) {
+      if(error instanceof HttpException) {
+        throw error;
+      }
+      throw new InternalServerErrorException;
     }
   }
 }
